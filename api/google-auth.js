e3a9f900ec58ab15cb116c33f98a5a1c570f7be6
Amine Hamou <aminehamou2101@gmail.com>
2026-09-03 18:26:51 +0100

    Simplify GET response in google-auth.js

--- api/google-auth.js
+++ api/google-auth.js
@@ -1,14 +1,8 @@
 export function GET() {
-  return new Response(
-    JSON.stringify({
-      success: true,
-      message: "HAMOU MATH API يعمل بنجاح"
-    }),
-    {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json"
-      }
+  return new Response("HAMOU MATH API OK", {
+    status: 200,
+    headers: {
+      "Content-Type": "text/plain; charset=utf-8"
     }
-  );
+  });
 }
