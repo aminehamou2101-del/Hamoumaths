4520f69906009f33c045541cb2fada311dccc8a2
Amine Hamou <aminehamou2101@gmail.com>
2026-09-05 23:51:08 +0100

    Create health.js

---
+++ functions/api/health.js
@@ -0,0 +1,16 @@
+export async function onRequestGet() {
+  return new Response(
+    JSON.stringify({
+      success: true,
+      service: "HAMOU MATH GLOBAL",
+      status: "online"
+    }),
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+        "Cache-Control": "no-store"
+      }
+    }
+  );
+}
