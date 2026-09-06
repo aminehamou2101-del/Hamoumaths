a75b130cd8040aa9d0c1bbf4d5a55dfa06d5e3f0
Amine Hamou <aminehamou2101@gmail.com>
2026-09-06 07:39:23 +0100

    Add configuration constants for application settings

---
+++ js/config.js
@@ -0,0 +1,14 @@
+export const CONFIG = Object.freeze({
+  SUPABASE_URL:
+    window.HAMOU_CONFIG?.SUPABASE_URL || "",
+
+  SUPABASE_ANON_KEY:
+    window.HAMOU_CONFIG?.SUPABASE_ANON_KEY || "",
+
+  OWNER_EMAIL:
+    "aminehamou2101@gmail.com",
+
+  RESOURCE_PAGE_SIZE: 24,
+
+  DEFAULT_LANGUAGE: "ar"
+});
